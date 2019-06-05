12515420dc73ef06c5b9174c5923bb0c70801696
Santosh Acharya <santosh.acharya1200@gmail.com>
2019-06-05 17:10:24 +0545

    Implemented LiveData Binding from the xml file to viewModel.

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -55,30 +55,34 @@ class GameFragment : Fragment() {
 
         viewModel= ViewModelProviders.of(this).get(GameViewModel::class.java)
 
+        binding.gameViewModel = viewModel
+
+        binding.setLifecycleOwner(this)
 
 
-        binding.correctButton.setOnClickListener { viewModel.
-                onCorrect()
 
-            }
-        binding.skipButton.setOnClickListener {
-            viewModel.onSkip()
+//        binding.correctButton.setOnClickListener { viewModel.
+//                onCorrect()
+//
+//            }
+//        binding.skipButton.setOnClickListener {
+//            viewModel.onSkip()
+//
+//
+//        }
 
-
-        }
-
-        viewModel.score.observe(this, Observer { newScore ->
-            binding.scoreText.text = newScore.toString()
-        })
+//        viewModel.score.observe(this, Observer { newScore ->
+//            binding.scoreText.text = newScore.toString()
+//        })
 
         viewModel.currentTime.observe(this, Observer { newTime ->
             binding.timerText.text = DateUtils.formatElapsedTime(newTime)
 
         })
 
-        viewModel.word.observe(this, Observer { newWord ->
-            binding.wordText.text = newWord
-        })
+//        viewModel.word.observe(this, Observer { newWord ->
+//            binding.wordText.text = newWord
+//        })
 
         viewModel.eventGameFinish.observe(this, Observer { hasFinished ->
             if(hasFinished){
